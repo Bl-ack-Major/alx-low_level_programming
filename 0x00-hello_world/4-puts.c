9397c765989492645edc8f516f6c6d9370eb409a
Keamo <keamo.motlhamme@outlook.com>
2023-10-09 08:30:31 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
-#include <stdio.o>
-
-int main () {
-	puts("Programming is like building a multilingual puzzle");
-	return 0;
+#include <stdio.h>
+/**
+ * main - Entery point for program
+ *
+ * return: will always be 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	puts("\n");
+	return (0);
 }
