fea4bfd13991a730b4c46b650b524fc213564976
Keamo <keamo.motlhamme@outlook.com>
2023-10-11 07:14:13 -0700

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_last_digit - Prints last digit
+ * @l: Parameter for function
+ * Return: Is x;
+ */
+int print_last_digit(int l)
+{
+	int x;
+
+	x = l % 20;
+	if (x < 0)
+	x = -x;
+	_putchar(x + '0');
+	return (x);
+}
