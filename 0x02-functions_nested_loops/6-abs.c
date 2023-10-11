e3360af2ec3cb1a287780dc4462e3f5a503ab529
Keamo <keamo.motlhamme@outlook.com>
2023-10-11 06:04:37 -0700

    absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - Computes absolute value
+ * @c: Parameeter for function
+ * Return: Always -c or c;
+ */
+int _abs(int c)
+{
+	if (c < 0)
+		return (-c);
+	else
+		return (c);
+}
