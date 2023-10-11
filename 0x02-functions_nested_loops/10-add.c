31489d84ead2dad80ac701975bb3e27a685e2206
Keamo <keamo.motlhamme@outlook.com>
2023-10-11 12:52:26 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - Function that adds two integers
+ * @a: First intteger to be added
+ * @b: second integer to be adder
+ * Return: sum a + b
+ */
+int add(int a, int b)
+{
+return (a + b);
+}
