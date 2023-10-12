ffc5a4ef7f9dcfe13ee933fb8e3131fd75328857
Keamo <keamo.motlhamme@outlook.com>
2023-10-12 04:38:40 -0700

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - Function the multiplies
+ * @a: Parameter for function
+ * @b: Parameter for function
+ * Return: Always a * b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
