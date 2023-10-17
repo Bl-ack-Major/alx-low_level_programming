44f84a022bb33c4c6b07cd89cc3a5f187071ac6e
Keamo <keamo.motlhamme@outlook.com>
2023-10-16 20:45:10 -0700

    puts 2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,15 +6,12 @@
  */
 void puts2(char *str)
 {
-	int i = 0;
+	int i;
 
-	if (str[0] != '\0')
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		while (str[i] != '\0')
-		{
-		_putchar(str[i]);
-		i += 2;
-		}
+		if (i % 2 == 0)
+			_putchar(str[i]);
 	}
 	_putchar('\n');
 }
