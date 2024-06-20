4a831e6375756c366ab5719ee0b56261fe6afe9d
Bl-ack-Major <keamo.motlhamme@outlook.com>
2024-06-20 16:55:19 -0700

    New Changes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
- * main- Starting point of program
  *
- * return: will always be 0
+ * main: this is the main function
+ *
+ * return: will be 0
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	puts("\n");
 	return (0);
 }
