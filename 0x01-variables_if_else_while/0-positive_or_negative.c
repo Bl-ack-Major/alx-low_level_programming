edbcf327c10527a8d98d8e5f7d32c7835222072a
Bl-ack-Major <keamo.motlhamme@outlook.com>
2024-06-21 14:45:12 -0700

    Betty fix

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,7 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
 #include <stdio.h>
-/* betty style doc for function main goes there */
-/**
+/*
  * main - main function
  *
  *Return: always 0
